7e37e4c16742c8e66685bd53e94fece3d34695ff
Oswaldo Rosales <oswaldo.rosales.a@gmail.com>
2020-06-25 16:29:27 -0500

    Feature: Add previous recipes app

--- recipe-app-full/src/App.js
+++ recipe-app-full/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import recipes from './data/recipes.json'
+import Recipe from "./components/Recipe";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <article>
+            <header>
+                <h1>Delicious Recipes</h1>
+            </header>
+            <div className="recipes">
+                {recipes.map((recipe, i) => (<Recipe key={i} {...recipe}/>))}
+            </div>
+        </article>
+    );
 }
 
 export default App;
